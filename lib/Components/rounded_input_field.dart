6a24c12ee630ba2538d183f58c4549874f996e16
Arya <aarryyaa292@gmail.com>
2021-09-19 07:43:01 +0530

    added welcome screen

---
+++ lib/Components/rounded_input_field.dart
@@ -0,0 +1,34 @@
+// @dart=2.9
+import 'package:flutter/material.dart';
+import 'package:ambulance_tracker/Components/text_field_container.dart';
+import 'package:ambulance_tracker/constants.dart';
+
+class RoundedInputField extends StatelessWidget {
+  final String hintText;
+  final IconData icon;
+  final ValueChanged<String> onChanged;
+  const RoundedInputField({
+    Key key,
+    this.hintText,
+    this.icon = Icons.person,
+    this.onChanged,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFieldContainer(
+      child: TextField(
+        onChanged: onChanged,
+        cursorColor: kPrimaryColor,
+        decoration: InputDecoration(
+          icon: Icon(
+            icon,
+            color: kPrimaryColor,
+          ),
+          hintText: hintText,
+          border: InputBorder.none,
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/Screens/Signup/signup_screen.dart
@@ -0,0 +1,22 @@
+// @dart=2.9
+import 'package:flutter/material.dart';
+import 'package:ambulance_tracker/constants.dart';
+import 'package:ambulance_tracker/Screens/Login/login_screen.dart';
+import 'package:ambulance_tracker/Components/already_have_an_account_acheck.dart';
+import 'package:ambulance_tracker/Components/rounded_button.dart';
+import 'package:ambulance_tracker/Components/rounded_input_field.dart';
+import 'package:ambulance_tracker/Components/rounded_password_field.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+
+class SignUpScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    //  body: Body(),
+    );
+  }
+}
+
+
